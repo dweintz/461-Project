840431e5a207895997ca9cc19837473ac0ef6c3c
dweintz <dweintz@purdue.edu>
2025-09-26 15:26:16 -0400

    Added tests for code quality metric

--- tests/test_code_quality.py
+++ tests/test_code_quality.py
@@ -1,12 +1,12 @@
 '''
-Test performance_claims.py
+Test code_quality.py
 '''
 
 import os
 import pytest
 from dotenv import load_dotenv
 from pathlib import Path
-from src.scorer.metrics.performance_claims import get_performance_claims
+from src.scorer.metrics.code_quality import get_code_quality
 
 @pytest.fixture(scope = "session", autouse = True)
 def load_env():
@@ -20,30 +20,14 @@ def load_env():
         raise RuntimeError("HF_TOKEN not found")
     return hf_token
 
-def test_model_url(load_env):
-    '''
-    Test performance claims on a Hugging Face URL
-    '''
-
-    url_model = f"https://huggingface.co/bert-base-uncased"
-    url_type_model = "model"
-    score, latency = get_performance_claims(url_model, url_type_model)
-
-    print(f"Model: Score = {score:.2f}, Latency ={latency}ms")
-
-    assert isinstance(score, (float))
-    assert isinstance(latency, (int))
-    assert 0.0 <= score <= 1.0
-    assert latency > 0
-
 def test_code_url(load_env):
     '''
     Test performance claims on a code URL
     '''
 
-    url_code = "https://github.com/pallets/flask"
+    url_code = "https://github.com/google-research/bert"
     url_type_code = "code"
-    score, latency = get_performance_claims(url_code, url_type_code)
+    score, latency = get_code_quality(url_code, url_type_code)
 
     print(f"Code: Score = {score:.2f}, Latency = {latency}ms")
 
